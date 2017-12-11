ec626023ebb58576da86da8cf03d5237189008b9
Jarosław Jantura <jjantura@gmail.com>
2017-12-11 18:46:52 +0100

    Add doubleEveryOther function tests

--- homework1/test/LibSpec.hs
+++ homework1/test/LibSpec.hs
@@ -4,7 +4,7 @@ import           Lib
 import           Test.Hspec
     
 spec :: Spec
-spec = 
+spec = do
     describe "toDigits" $ do
         it "negative should be empty list" $ toDigits (-17) `shouldBe` []
         it "zero should be empty list" $ toDigits 0 `shouldBe` []
@@ -15,3 +15,7 @@ spec =
         it "zero should be empty list" $ toDigitsRev 0 `shouldBe` []
         it "1234 should be [4,3,2,1]" $ toDigitsRev 1234 `shouldBe` [4,3,2,1]
         it "1230 should be [0,3,2,1]" $ toDigitsRev 1230 `shouldBe` [0,3,2,1]
+    describe "doubleEveryOther" $ do
+        it "[8,7,6,5] should be [16,7,12,5]" $ doubleEveryOther [8,7,6,5] `shouldBe` [16,7,12,5]
+        it "[1,2,3] should be [1,4,3]" $ doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
+        
