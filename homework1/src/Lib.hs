223abbf3fc3706bb96cc02ef06ecd9c3c4e6adc6
Jarosław Jantura <jjantura@gmail.com>
2017-12-11 18:29:07 +0100

    Add doubleEveryOther placeholder

--- homework1/src/Lib.hs
+++ homework1/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     ( toDigits,
-    toDigitsRev
+    toDigitsRev,
+    doubleEveryOther
     ) where
 
 -- exercise 1
@@ -10,3 +11,6 @@ toDigits n = reverse $ toDigitsRev n
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n | n <= 0 = [] | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10) 
 
+-- exercise2
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = undefined
