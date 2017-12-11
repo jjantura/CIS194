044c3ce0e5747264f2045823fad4debb6db24158
Jarosław Jantura <jjantura@gmail.com>
2017-12-11 18:46:43 +0100

    Add doubleEveryOther function

--- homework1/src/Lib.hs
+++ homework1/src/Lib.hs
@@ -13,4 +13,4 @@ toDigitsRev n | n <= 0 = [] | otherwise = (n `mod` 10) : toDigitsRev (n `div` 10
 
 -- exercise2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther xs = foldl(\a e -> if fst e `mod` 2 == 0 then snd e:a else (2 * snd e) : a) [] (zip [0 .. (length xs) - 1] $ reverse xs) 
