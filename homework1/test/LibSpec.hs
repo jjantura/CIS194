2de46612cc8efdfbae9e44655b2a039d0044e4f4
Jaroslaw Jantura <jjantura@gmail.com>
2017-12-11 21:35:46 +0100

    Add sumDigits function tests

--- homework1/test/LibSpec.hs
+++ homework1/test/LibSpec.hs
@@ -6,16 +6,19 @@ import           Test.Hspec
 spec :: Spec
 spec = do
     describe "toDigits" $ do
-        it "negative should be empty list" $ toDigits (-17) `shouldBe` []
-        it "zero should be empty list" $ toDigits 0 `shouldBe` []
-        it "1234 should be [1,2,3,4]" $ toDigits 1234 `shouldBe` [1,2,3,4]
-        it "1230 should be [1,2,3,0]" $ toDigits 1230 `shouldBe` [1,2,3,0]
+        it "toDigits negative should be empty list" $ toDigits (-17) `shouldBe` []
+        it "toDigits 0 should be empty list" $ toDigits 0 `shouldBe` []
+        it "toDigits 1234 should be [1,2,3,4]" $ toDigits 1234 `shouldBe` [1,2,3,4]
+        it "toDigits 1230 should be [1,2,3,0]" $ toDigits 1230 `shouldBe` [1,2,3,0]
     describe "toDigitsRev" $ do
-        it "negative should be empty list" $ toDigitsRev (-17) `shouldBe` []
-        it "zero should be empty list" $ toDigitsRev 0 `shouldBe` []
-        it "1234 should be [4,3,2,1]" $ toDigitsRev 1234 `shouldBe` [4,3,2,1]
-        it "1230 should be [0,3,2,1]" $ toDigitsRev 1230 `shouldBe` [0,3,2,1]
+        it "toDigitsRev negative should be empty list" $ toDigitsRev (-17) `shouldBe` []
+        it "toDigitsRev 0 should be empty list" $ toDigitsRev 0 `shouldBe` []
+        it "toDigitsRev 1234 should be [4,3,2,1]" $ toDigitsRev 1234 `shouldBe` [4,3,2,1]
+        it "toDigitsRev 1230 should be [0,3,2,1]" $ toDigitsRev 1230 `shouldBe` [0,3,2,1]
     describe "doubleEveryOther" $ do
-        it "[8,7,6,5] should be [16,7,12,5]" $ doubleEveryOther [8,7,6,5] `shouldBe` [16,7,12,5]
-        it "[1,2,3] should be [1,4,3]" $ doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
+        it "doubleEveryOther [8,7,6,5] should be [16,7,12,5]" $ doubleEveryOther [8,7,6,5] `shouldBe` [16,7,12,5]
+        it "doubleEveryOther [1,2,3] should be [1,4,3]" $ doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
+    describe "sumDigits" $ do
+        it "sumDigits [1,2,3] should be 6" $ sumDigits [1,2,3] `shouldBe` 6
+        it "sumDigits [11,22,33] should be 12" $ sumDigits [11,22,33] `shouldBe` 12
         
