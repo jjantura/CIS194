9d1bbbedc1fe410757b64f4559a3dd49aa464c03
Jarosław Jantura <jjantura@gmail.com>
2017-12-18 21:08:18 +0100

    Add more tests

--- homework2/test/LogAnalysisSpec.hs
+++ homework2/test/LogAnalysisSpec.hs
@@ -10,4 +10,8 @@ spec =
         it "should return valid info message" $ parseMessage "I 123 text" `shouldBe` LogMessage Info 123 "text"
         it "should return valid warning message" $ parseMessage "W 123 text" `shouldBe` LogMessage Warning 123 "text"
         it "should return valid error message" $ parseMessage "E 100 123 text" `shouldBe` LogMessage (Error 100) 123 "text"
-                                
+
+        it "should return Unknown for plain string" $ parseMessage "The Magic Words are Squeamish Ossifrage" `shouldBe` Unknown "The Magic Words are Squeamish Ossifrage"
+        it "should return Unknown for invalid timestamp" $ parseMessage "I TS somestring" `shouldBe` Unknown "I TS somestring"
+        it "should return Unknown for empty string" $ parseMessage "" `shouldBe` Unknown ""
+        it "should return Unknown for invalid error level" $ parseMessage "E EL 120 meh" `shouldBe` Unknown "E EL 120 meh"
