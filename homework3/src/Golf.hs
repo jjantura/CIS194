5ddc2f3b5e968a4a675501cf99f4f2871b9cd98d
Jarosław Jantura <jjantura@gmail.com>
2017-12-24 09:35:21 +0100

    Add solution for exercise 2 - localMaxima

--- homework3/src/Golf.hs
+++ homework3/src/Golf.hs
@@ -1,11 +1,16 @@
 module Golf
     ( 
-        skips
+        skips,
+        localMaxima
     ) where
 
+-- exercise 1
 -- picks mod n elements n = 1 - every, n = 2 even and so on
 picks n xs = foldr (\e a -> if (fst e `mod` n) == 0 then snd e:a else a) [] (zip [1..] xs)
 
-
 skips :: [a] -> [[a]]
 skips xs = foldr (\e a -> picks e xs:a) [] [1..length xs]
+
+-- exercise 2
+localMaxima :: [Integer] -> [Integer]
+localMaxima n = [ n !! i | i <-[1..length n - 2], n !! i > n !! (i - 1) && n !! i > n !! (i + 1)]
