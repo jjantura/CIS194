6761bd9be4853566ef1f9f2b4b345265e5a57aa4
Jarosław Jantura <jjantura@gmail.com>
2017-12-30 23:09:55 +0100

    Add helper functions for exercise 3

--- homework3/src/Golf.hs
+++ homework3/src/Golf.hs
@@ -4,7 +4,9 @@ module Golf
         localMaxima,
         count,
         histogram,
-        histogram2
+        histogram2,
+        maxHeight,
+        scanline
     ) where
 
 -- exercise 1
@@ -18,13 +20,20 @@ skips xs = foldr (\e a -> picks e xs:a) [] [1..length xs]
 localMaxima :: [Integer] -> [Integer]
 localMaxima n = [ n !! i | i <-[1..length n - 2], n !! i > n !! (i - 1) && n !! i > n !! (i + 1)]
 
--- exercise 3 - helper functionI
+-- exercise 3 - helper function, count number of occurences
 count :: [Integer] -> Integer -> Int
 count xs n = length $ filter(==n) xs 
-    --foldl (\a e -> if e == n then a + 1 else a) 0 xs
 
+-- helper functiom, calculate 'classic' histogram
 histogram2 :: [Integer] -> [(Integer, Int)]
 histogram2 xs = foldr (\e a -> (e, count xs e):a) [] [0..9] 
+
+-- convert classic histogram to a line
+scanline :: Integer -> [Integer] -> String
+scanline n xs = foldl (\a e -> if e <= n then '*':a else ' ':a) [] xs
+
+maxHeight :: [Integer] -> Int
+maxHeight xs = maximum $ map (snd) $ histogram2 xs
 
 -- exercise 3
 histogram :: [Integer] -> String
