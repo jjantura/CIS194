9d5e23c311916d96cba98913ad906335ea5b14a6
Jarosław Jantura <jjantura@gmail.com>
2017-12-30 23:27:01 +0100

    Add solution (still not perfect) for exercise 3

--- homework3/src/Golf.hs
+++ homework3/src/Golf.hs
@@ -21,21 +21,22 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima n = [ n !! i | i <-[1..length n - 2], n !! i > n !! (i - 1) && n !! i > n !! (i + 1)]
 
 -- exercise 3 - helper function, count number of occurences
-count :: [Integer] -> Integer -> Int
-count xs n = length $ filter(==n) xs 
+count :: [Integer] -> Integer -> Integer
+count xs n = fromIntegral $ length $ filter(==n) xs 
 
 -- helper functiom, calculate 'classic' histogram
-histogram2 :: [Integer] -> [(Integer, Int)]
+histogram2 :: [Integer] -> [(Integer, Integer)]
 histogram2 xs = foldr (\e a -> (e, count xs e):a) [] [0..9] 
 
 -- convert classic histogram to a line
 scanline :: Integer -> [Integer] -> String
-scanline n xs = foldl (\a e -> if e <= n then '*':a else ' ':a) [] xs
+scanline n xs = foldr (\e a -> if n <= e then '*':a else ' ':a) [] xs
 
-maxHeight :: [Integer] -> Int
+maxHeight :: [Integer] -> Integer
 maxHeight xs = maximum $ map (snd) $ histogram2 xs
 
 -- exercise 3
 histogram :: [Integer] -> String
-histogram xs = "\n==========\n0123456789\n"
+histogram xs = scanAll ++ "\n==========\n0123456789\n"
+        where scanAll = foldl(\a e -> "\n" ++ (scanline e $ map (snd) $ histogram2 xs)  ++ a) [] [1..maxHeight xs]
 
