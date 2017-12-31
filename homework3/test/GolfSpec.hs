9cc0a0d39f44e0f7859373f924214ec3d11a5a3f
Jarosław Jantura <jjantura@gmail.com>
2017-12-31 09:00:25 +0100

    Add tests for histogram, fix tests for localMaxima

--- homework3/test/GolfSpec.hs
+++ homework3/test/GolfSpec.hs
@@ -14,7 +14,8 @@ spec = do
 --        it "test 5" $ skips [] `shouldBe` []
     describe "localMaxima" $ do
         it "test 1" $ localMaxima [2,9,5,6,1] `shouldBe` [9,6]
-        it "test 2" $ [2,3,4,1,5] `shouldBe` [4]
-        it "test 3" $ [1,2,3,4,5] `shouldBe` []
-    
-
+        it "test 2" $ localMaxima [2,3,4,1,5] `shouldBe` [4]
+        it "test 3" $ localMaxima [1,2,3,4,5] `shouldBe` []
+    describe "histogram" $ do
+        it "test 1" $ histogram [1,1,1,5] `shouldBe` "\n *        \n *        \n *   *    \n==========\n0123456789\n"
+        it "test 2" $ histogram [1,4,5,4,6,6,3,4,2,4,9] `shouldBe` "\n    *     \n    *     \n    * *   \n ******  *\n==========\n0123456789\n"
