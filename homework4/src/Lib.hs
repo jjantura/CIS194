c5b1dea0a41747ad4892c3d5c60c81da2ce1c20c
Jarosław Jantura <jjantura@gmail.com>
2018-01-05 20:55:53 +0100

    Add fun'2 - sum of all even numbers in Collatz sequence

--- homework4/src/Lib.hs
+++ homework4/src/Lib.hs
@@ -11,8 +11,10 @@ fun1 (x:xs) | even x = (x - 2) * fun1 xs | otherwise = fun1 xs
 
 fun1' = product . map(\x -> x - 2) . filter (even)
 
+-- sum of all even numbers in collatz sequenceS
 fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 n | even n = n + fun2 (n `div` 2) | otherwise = fun2 (3 * n + 1)
 
-fun2' = undefined
+fun2' :: Integer -> Integer
+fun2' n = sum $ filter even $ takeWhile (/=1) $ iterate (\x -> if even x then x `div` 2 else 3 * x + 1) n
