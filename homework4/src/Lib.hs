c5ef936c2ba456076e6011ab775ed90b6611d99c
Jarosław Jantura <jjantura@gmail.com>
2018-01-05 21:09:04 +0100

    Add exercise2 placeholder

--- homework4/src/Lib.hs
+++ homework4/src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
     fun2'
     ) where
 
+-- exercise 1
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x:xs) | even x = (x - 2) * fun1 xs | otherwise = fun1 xs
@@ -18,3 +19,9 @@ fun2 n | even n = n + fun2 (n `div` 2) | otherwise = fun2 (3 * n + 1)
 
 fun2' :: Integer -> Integer
 fun2' n = sum $ filter even $ takeWhile (/=1) $ iterate (\x -> if even x then x `div` 2 else 3 * x + 1) n
+
+-- exercise 2
+data Tree a = Leaf | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
+foldTree :: [a] -> Tree a
+foldTree = undefined
+
