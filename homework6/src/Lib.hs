53ed32ef9da529ee61f69e1cb5296a45405a533a
Jarosław Jantura <jjantura@gmail.com>
2018-02-27 21:37:02 +0100

    Add fibonacci function

--- homework6/src/Lib.hs
+++ homework6/src/Lib.hs
@@ -1,6 +1,6 @@
 module Lib
-    ( someFunc
+    ( fib
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+fib :: Integer -> Integer
+fib n = if n == 0 || n == 1  then 1 else fib(n - 1) + fib(n - 2)
