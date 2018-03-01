da25c798cf67f93c63b0533ab37826de0042b736
Jarosław Jantura <jjantura@gmail.com>
2018-03-01 18:02:30 +0100

    Add solution for exercise 2

--- homework6/src/Lib.hs
+++ homework6/src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( fib, 
-    fibs1
+    fib2, 
+    fibs1,
+    fibs2
     ) where
 
 -- exercise 1
@@ -9,3 +11,12 @@ fib n = if n == 0 || n == 1  then 1 else fib(n - 1) + fib(n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0..]
+
+-- exercise 2
+
+fib2 :: Integer -> Integer -> [Integer]
+fib2 x y = x : fib2 y (x + y)
+
+fibs2 :: [Integer]
+fibs2 = fib2 1 1
+
